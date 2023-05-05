7f63add0c1281a251fe57efcf25c88937044e9e7
Seckin Ersoy <seckinersoy06@gmail.com>
2023-05-05 05:29:38 +0300

    first commit

---
+++ state/src/Course.jsx
@@ -0,0 +1,24 @@
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Ccsharp from "./images/ccsharp.png";
+import KompleWeb from "./images/kompleweb.jpg";
+import './Course.css';
+
+const courseMap = {
+  Angular,
+  Bootstrap,
+  Ccsharp,
+  KompleWeb,
+};
+
+// eslint-disable-next-line react/prop-types
+function Course({ courseName }) {
+  console.log(courseMap[courseName]);
+  return (
+    <div className="courseDiv">
+      <img className="course" src={courseMap[courseName]} alt="course" />
+    </div>
+  );
+}
+
+export default Course;
